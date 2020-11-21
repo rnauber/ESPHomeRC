831563ddae17c085671a77de35122e441d9ec171
rnauber <richard@nauber.dev>
2020-11-21 17:52:12 +0100

    Refactor...

--- app/src/androidTest/java/dev/nauber/esphomerc/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/dev/nauber/esphomerc/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("dev.nauber.esphomerccar", appContext.packageName)
+        assertEquals("dev.nauber.esphomerc", appContext.packageName)
     }
 }
